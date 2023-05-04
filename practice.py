07816b2645b80841a7757016617c49c9f27084d6
Ayobami <ayobamidele006@gmail.com>
2023-05-04 15:54:07 +0100

    hbtn server preparation

--- practice.py
+++ practice.py
@@ -1,54 +1,54 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 
-import re
+# import re
 
-s = 'Sate name="New York" email="toronto dsdks" number=0812237823"'
+# s = 'Sate name="New York" email="toronto dsdks" number=0812237823"'
 
-# # Use regular expression pattern to find double-quoted substrings
-# pattern = r'"[^"]+"'
-# matches = re.findall(pattern, s)
+# # # Use regular expression pattern to find double-quoted substrings
+# # pattern = r'"[^"]+"'
+# # matches = re.findall(pattern, s)
 
-# # Replace double-quoted substrings with placeholders
-# for i, match in enumerate(matches):
-#     s = s.replace(match, f'###{i}###')
+# # # Replace double-quoted substrings with placeholders
+# # for i, match in enumerate(matches):
+# #     s = s.replace(match, f'###{i}###')
 
-# # Split the string based on '=' and whitespace outside of double quotes
-# split_parts = re.split(r'=\s+|(?:"[^"]+")', s)
+# # # Split the string based on '=' and whitespace outside of double quotes
+# # split_parts = re.split(r'=\s+|(?:"[^"]+")', s)
 
-# # Replace placeholders with original double-quoted substrings
-# for i, part in enumerate(split_parts):
-#     if part.startswith('###') and part.endswith('###'):
-#         index = int(part[3:-3])
-#         split_parts[i] = matches[index]
+# # # Replace placeholders with original double-quoted substrings
+# # for i, part in enumerate(split_parts):
+# #     if part.startswith('###') and part.endswith('###'):
+# #         index = int(part[3:-3])
+# #         split_parts[i] = matches[index]
 
-# print(split_parts)
+# # print(split_parts)
 
-pattern = r"^(?P<name>State)"
-param_pattern = r"(?P<params>\w+=(\"[\w\d ]+\"|\d+))"
-
-
-jj = re.compile(pattern)
-pp = re.compile(param_pattern)
-
-print(jj.findall(s))
-print(pp.findall(s))
-#    print(data[0].split("="))
+# pattern = r"^(?P<name>State)"
+# param_pattern = r"(?P<params>\w+=(\"[\w\d ]+\"|\d+))"
 
 
-# print(split_parts)
+# jj = re.compile(pattern)
+# pp = re.compile(param_pattern)
+
+# print(jj.findall(s))
+# print(pp.findall(s))
+# #    print(data[0].split("="))
 
 
-class New:
-    # user = "Ayo"
-
-    def __init__(self):
-        self.name = "something"
-        self.email = "toronto dsdks"
-        self.number = "0812237823"
-        user = "Ayo"
-        self.user = user
+# # print(split_parts)
 
 
-new = New()
+# class New:
+#     # user = "Ayo"
 
-print(New.__name__)
+#     def __init__(self):
+#         self.name = "something"
+#         self.email = "toronto dsdks"
+#         self.number = "0812237823"
+#         user = "Ayo"
+#         self.user = user
+
+
+# new = New()
+
+# print(New.__name__)
